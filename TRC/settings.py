cf0cefc5415d83b5603e3ef018ca47cee0b6b66c
amir abbas <amirabasf230@gmail.com>
2024-11-13 15:47:37 +0330

    add http_host

--- TRC/settings.py
+++ TRC/settings.py
@@ -123,3 +123,6 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+
+HTTP_HOST = ['amir900abbas.pythonanywhere.com/']
